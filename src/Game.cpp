6905151627f697b8600d9783a2be20f06eeec5c5
Felipe Góes <ogoes.felipe@gmail.com>
2022-06-13 22:18:29 -0300

    Enemy & Mouse positions.

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,6 +5,7 @@ Game::Game()
 {
   initVariables();
   initWindow();
+  initEnemies();
 }
 
 Game::~Game() { delete this->window; }
@@ -18,6 +19,18 @@ void Game::initWindow(void)
 
   this->window = new sf::RenderWindow(videoMode, "My first game",
                                       sf::Style::Titlebar | sf::Style::Close);
+
+  this->window->setFramerateLimit(60);
+}
+
+void Game::initEnemies()
+{
+  this->enemy.setPosition(10.f, 10.f);
+  this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+  this->enemy.setScale(sf::Vector2f(0.5f, 0.5f));
+  this->enemy.setFillColor(sf::Color::Cyan);
+  this->enemy.setOutlineColor(sf::Color::Green);
+  this->enemy.setOutlineThickness(1.f);
 }
 
 void Game::pollEvent(void)
@@ -43,7 +56,18 @@ void Game::pollEvent(void)
 const bool Game::running(void) const { return this->window->isOpen(); }
 
 // Functions
-void Game::update(void) { pollEvent(); }
+void Game::update(void)
+{
+  pollEvent();
+
+  // Update mouse position
+  // Relative to the screen
+  // std::cout << "Mouse pos: " << sf::Mouse::getPosition().x << " "
+  //           << sf::Mouse::getPosition().y << std::endl;
+  // Relative to the window
+  std::cout << "Mouse pos: " << sf::Mouse::getPosition(*this->window).x << " "
+            << sf::Mouse::getPosition(*this->window).y << std::endl;
+}
 
 void Game::render(void)
 {
@@ -59,6 +83,7 @@ void Game::render(void)
   this->window->clear(sf::Color::Blue); // Window Clear old frame
 
   // Draw game objects
+  this->window->draw(this->enemy);
 
   this->window->display(); // Tell app that window is done drawing
 }
--- src/include/Game.hpp
+++ src/include/Game.hpp
@@ -1,20 +1,31 @@
 #pragma once
 
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 
 class Game
 {
 private:
+  // Variables
+  // Window
   sf::RenderWindow *window;
   sf::VideoMode videoMode;
   sf::Event ev;
 
+  // Game objects
+  sf::RectangleShape enemy;
+
+  // Private functions
   void initWindow(void);
   void initVariables(void);
   void pollEvent(void);
+  void initEnemies(void);
 
 public:
+  // Constructors | Destructors
   Game();
   ~Game();
 
